ab517562e1e368b06cf7459064d428e0e4e2b38d
d3val <devalt5008@gmail.com>
2022-11-15 14:50:31 -0600

    Heat script update
    - Heat objects now are able to interact with other Heat objects and 'transfer' heat.

--- Assets/Scripts/Heat.cs
+++ Assets/Scripts/Heat.cs
@@ -5,9 +5,53 @@ using UnityEngine;
 public class Heat : MonoBehaviour
 {
     public bool isHot = false;
-   
+    public bool isHeatSource = false;
+    Heat currentHeatSource = null;
+
+    private void Awake()
+    {
+        isHeatSource = false;
+    }
+    private void Update()
+    {
+        if (isHeatSource)
+            return;
+        if (currentHeatSource == null)
+        {
+            isHot = false;
+            return;
+        }
+
+        isHot = currentHeatSource.isHot;
+    }
+
     public void ToogleHeat()
     {
         isHot = !isHot;
     }
+
+    public void ToogleHeatSource()
+    {
+        isHeatSource = !isHeatSource;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Fire Source"))
+            return;
+
+        currentHeatSource = collision.gameObject.GetComponent<Heat>();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+
+        if (!collision.gameObject.CompareTag("Fire Source"))
+            return;
+
+        currentHeatSource = null;
+
+    }
+
 }
